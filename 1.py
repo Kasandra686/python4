f7a47774e6be9507c1394f184cd356a355e2ac86
Вера <verazverev@yandex.ru>
2020-01-26 23:34:14 +0500

    1

---
+++ 1.py
@@ -0,0 +1,7 @@
+from sys import argv
+try:
+    script_name,hour, salary, prize = argv
+    result=( int(hour) * int(salary)) + int(prize)
+    print(result)
+except ValueError:
+    print ("Необходимо вводить три числа!")
