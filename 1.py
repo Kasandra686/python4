213b7ac44618a4807fc420fbd71d0a6d321d8f8d
Вера <verazverev@yandex.ru>
2020-01-26 23:34:50 +0500

    1

--- 1.py
+++ 1.py
@@ -1,7 +1,8 @@
 from sys import argv
+
 try:
-    script_name,hour, salary, prize = argv
-    result=( int(hour) * int(salary)) + int(prize)
+    script_name, hour, salary, prize = argv
+    result = (int(hour) * int(salary)) + int(prize)
     print(result)
 except ValueError:
-    print ("Необходимо вводить три числа!")
+    print("Необходимо вводить три числа!")
